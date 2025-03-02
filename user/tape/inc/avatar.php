72ef091364905e83415c9d8e37e25f64825b3586
guguan123 <guguanming@outlook.com>
2025-03-02 10:46:02 +0800

    ... 看起来是修好了，但是不知道是怎么修的

--- user/tape/inc/avatar.php
+++ user/tape/inc/avatar.php
@@ -2,7 +2,7 @@
 /*
 * $name 个体操作描述 
 */
-if ($type == 'avatar' && $post['avtor'] != $user['id']) {	// 阿凡达
+if ($type == 'avatar' && $post['avtor'] != $user['id']) {
 	if ($post['avatar']) {
 		$name = '修改了' . ($avtor['pol'] == 1 ? null : "а") . ' 主页上的照片';
 	} else {
@@ -14,31 +14,35 @@ if ($type == 'avatar' && $post['avtor'] != $user['id']) {	// 阿凡达
 * 内容块输出 
 */
 if ($type == 'avatar') {
-	$photo = dbassoc(dbquery("SELECT * FROM `gallery_photo` WHERE `id` = '" . $post['id_file'] . "' LIMIT 1"));
-	$avatar = dbassoc(dbquery("SELECT * FROM `gallery_photo` WHERE `id` = '" . $post['avatar'] . "' LIMIT 1"));
-	$gallery = dbassoc(dbquery("SELECT * FROM `gallery` WHERE `id` = '" . $photo['id_gallery'] . "' LIMIT 1"));
-	if (isset($avatar['id_gallery'])) $gallery2 = dbassoc(dbquery("SELECT * FROM `gallery` WHERE `id` = '" . $avatar['id_gallery'] . "' LIMIT 1"));
 	echo '<div class="nav1">';
 	echo  user::nick($avtor['id'],1,1,0);
-	echo medal($avtor['id']) . ' <a href="user.settings.php?id=' . $avtor['id'] . '">[!]</a> ' . $name;
-	echo $s1 . vremja($post['time']) . $s2;
+	echo medal($avtor['id']) . ' <a href="user.settings.php?id=' . $avtor['id'] . '">[!]</a> ' . $name . ' ' . $s1 . vremja($post['time']) . $s2;
 	echo '</div>';
 	echo '<div class="nav2">';
-	if ($photo['id']) echo '<b>' . text($photo['name']) . '</b>';
-	if (isset($avatar['id']) && $avatar['id']) echo ' &raquo; <b>' . text($avatar['name']) . '</b>';
-	if (isset($avatar['id']) && $avatar['id'] || $photo['id'])echo '<br />';
 
+	// 获取新头像的图片信息
+	$photo = dbassoc(dbquery("SELECT * FROM `gallery_photo` WHERE `id` = '" . $post['id_file'] . "' LIMIT 1"));
+	if (isset($photo['id']) && $photo['id']) {
+		$gallery = dbassoc(dbquery("SELECT * FROM `gallery` WHERE `id` = '" . $photo['id_gallery'] . "' LIMIT 1"));
+		echo '<b>' . text($photo['name']) . '</b>';
+	}
+
+	// 展示以前的头像
 	if ($post['avatar']) {
-		if ($avatar['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery2['id'] . '/' . $avatar['id'] . '/">';
+		$avatar = dbassoc(dbquery("SELECT * FROM `gallery_photo` WHERE `id` = '" . $post['avatar'] . "' LIMIT 1"));
+		if (isset($avatar['id_gallery'])) $gallery2 = dbassoc(dbquery("SELECT * FROM `gallery` WHERE `id` = '" . $avatar['id_gallery'] . "' LIMIT 1"));
+		if (isset($avatar['id']) && $avatar['id']) echo ' &raquo; <b>' . text($avatar['name']) . '</b>';
+		if (isset($avatar['id']) && $avatar['id'] || (isset($photo['id']) && $photo['id'])) echo '<br />';
+		if (isset($avatar['id']) && $avatar['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery2['id'] . '/' . $avatar['id'] . '/">';
 		echo '<img style="max-width:50px; margin:3px;" src="/photo/photo50/' . $post['avatar'] . '.jpg" alt="*" />';
-		if ($avatar['id']) echo '</a>';
+		if (isset($avatar['id']) && $avatar['id']) echo '</a>';
 		echo ' <img src="/style/icons/arRt2.png" alt="*"/> ';
 	}
 
-	if ($photo['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery['id'] . '/' . $photo['id'] . '/">';
+	if (isset($photo['id']) && $photo['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery['id'] . '/' . $photo['id'] . '/">';
 	echo '<img style=" max-width:50px; margin:3px;" src="/photo/photo50/' . $post['id_file'] . '.jpg" alt="*" />';
-	if ($photo['id']) echo '</a>';
+	if (isset($photo['id']) && $photo['id']) echo '</a>';
 
 	echo '<br />';
-	if ($photo['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery['id'] . '/' . $photo['id'] . '/"><img src="/style/icons/bbl5.png" alt="*"/> (' . dbresult(dbquery("SELECT COUNT(*) FROM `gallery_komm` WHERE `id_photo` = '$photo[id]'"),0) . ')</a> ';
+	if (isset($photo['id']) && $photo['id']) echo '<a href="/photo/' . $avtor['id'] . '/' . $gallery['id'] . '/' . $photo['id'] . '/"><img src="/style/icons/bbl5.png" alt="*"/> (' . dbresult(dbquery("SELECT COUNT(*) FROM `gallery_komm` WHERE `id_photo` = '$photo[id]'"),0) . ')</a> ';
 }
--- user/tape/inc/files.php
+++ user/tape/inc/files.php
@@ -1,10 +1,11 @@
-<?
+<?php
 /*
 * $name 个体操作描述 
 */
 if ($type == 'down' && $post['avtor'] != $user['id']) {
 	$name = '文件夹中的新文件';
 }
+
 /*
 * 内容块输出 
 */
@@ -17,24 +18,29 @@ if ($type == 'down') {
 		$kol = $post['count'];
 	}
 	echo '<div class="nav1">';
-	echo  user::nick($avtor['id'], 1, 0, 0) .
-		' <a href="user.settings.php?id=' . $avtor['id'] . '">[!]</a> ' . $name . ' <img src="/style/themes/' . $set['set_them'] . '/loads/14/dir.png" alt="*"/> <a href="/user/personalfiles/' . $dir['id_user'] . '/' . $dir['id'] . '/">' . text($dir['name']) . '</a>  ' . $s1 . vremja($post['time']) . $s2;
+	echo  user::nick($avtor['id'], 1, 0, 0) . ' <a href="user.settings.php?id=' . $avtor['id'] . '">[!]</a> ' . $name . ' ';
+	if (isset($dir['id'])) {
+		echo '<img src="/style/themes/' . $set['set_them'] . '/loads/14/dir.png" alt="*"/> <a href="/user/personalfiles/' . $dir['id_user'] . '/' . $dir['id'] . '/">' . text($dir['name']) . '</a> ';
+	}
+	echo $s1 . vremja($post['time']) . $s2;
 	echo '</div>';
 	echo '<div class="nav2">';
-	$files = dbquery("SELECT * FROM `downnik_files` WHERE `my_dir` = '$dir[id]' ORDER BY `id` DESC LIMIT $kol");
-	while ($file = dbassoc($files)) {
-		if ($file['id']) {
-			$ras = $file['ras'];
-			if (is_file(H . 'style/themes/' . $set['set_them'] . '/loads/14/' . $ras . '.png')) // Иконка файла
-				echo '<img src="/style/themes/' . $set['set_them'] . '/loads/14/' . $ras . '.png" alt="*" /> ';
-			else
-				echo '<img src="/style/themes/' . $set['set_them'] . '/loads/14/file.png" alt="*" /> ';
-			echo '<a href="/user/personalfiles/' . $file['id_user'] . '/' . $dir['id'] . '/?id_file=' . $file['id'] . '&amp;page=1"><b>' . text($file['name']) . '.' . $ras . '</b></a> (' . size_file($file['size']) . ')<br />';
-		} else {
-			echo user::nick($avtor['id'], 0, 0, 0) . '  <a href="user.settings.php?id=' . $avtor['id'] . '">[!]</a>';
-			echo '<br />';
-			echo '该文件已被删除 =(<br />';
+	if (isset($dir['id'])) {
+		$files = dbquery("SELECT * FROM `downnik_files` WHERE `my_dir` = '$dir[id]' ORDER BY `id` DESC LIMIT $kol");
+		while ($file = dbassoc($files)) {
+			if ($file['id']) {
+				$ras = $file['ras'];
+				if (is_file(H . 'style/themes/' . $set['set_them'] . '/loads/14/' . $ras . '.png')) // Иконка файла
+					echo '<img src="/style/themes/' . $set['set_them'] . '/loads/14/' . $ras . '.png" alt="*" /> ';
+				else
+					echo '<img src="/style/themes/' . $set['set_them'] . '/loads/14/file.png" alt="*" /> ';
+				echo '<a href="/user/personalfiles/' . $file['id_user'] . '/' . $dir['id'] . '/?id_file=' . $file['id'] . '&amp;page=1"><b>' . text($file['name']) . '.' . $ras . '</b></a> (' . size_file($file['size']) . ')<br />';
+			} else {
+				echo '该文件已被删除 =(<br />';
+			}
 		}
+	} else {
+		echo '文件夹已被删除 =(<br />';
 	}
 	if (isset($kol2)) echo '和更多 ' . $kol2 . ' 档案';
 }
