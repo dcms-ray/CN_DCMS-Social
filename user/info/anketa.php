442ec00ddc78dc65b668421fa46a175afbf42c64
guguan123 <guguanming@outlook.com>
2025-03-03 11:07:07 +0800

    小修复

--- user/info/anketa.php
+++ user/info/anketa.php
@@ -165,7 +165,7 @@ if (isset($user) && $ank['id'] == $user['id']) {
 }
 if ($ank['group_access'] > 1) echo "<div class='err'>$ank[group_name]</div>";
 echo "<div class='nav2'>";
-echo "<span class=\"ank_n\">最后登录:</span> <span class=\"ank_d\">" . vremja($ank['date_last']) . "</span><br />";
+if (isset($ank['date_last'])) echo "<span class=\"ank_n\">最后登录:</span> <span class=\"ank_d\">" . vremja($ank['date_last']) . "</span><br />";
 echo "</div>";
 echo "<div class='nav1'>";
 echo user::avatar($ank['id'], true, 128, 128);
--- user/login.php
+++ user/login.php
@@ -7,7 +7,7 @@ include_once '../sys/inc/settings.php';
 include_once '../sys/inc/db_connect.php';
 include_once '../sys/inc/ipua.php';
 include_once '../sys/inc/fnc.php';
-$show_all=true;
+$show_all = true;
 include_once '../sys/inc/user.php';
 only_unreg();
 
