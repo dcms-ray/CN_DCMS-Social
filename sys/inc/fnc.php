c6a81e92002edd3ed08833ed7095a7f82e0ef888
guguan123 <guguanming@outlook.com>
2025-03-22 07:41:10 +0800

    登录历史添加最后在线时间

--- sys/inc/fnc.php
+++ sys/inc/fnc.php
@@ -179,16 +179,16 @@ function vremja($time = NULL) {
 	global $user;
 	if ($time == NULL) $time = time();
 	if (isset($user)) $time = $time + $user['set_timesdvig'] * 60 * 60;
-	$timep = "" . date("Y m d H:i", $time) . "";
-	$time_p[0] = date("Y m d", $time);
+	$timep = "" . date("Y/m/d H:i", $time) . "";
+	$time_p[0] = date("Y/m/d", $time);
 	$time_p[1] = date("H:i", $time);
-	if ($time_p[0] == date("Y m d")) $timep = date("H:i:s", $time);
+	if ($time_p[0] == date("Y/m/d")) $timep = date("H:i:s", $time);
 	if (isset($user)) {
-		if ($time_p[0] == date("Y m d", time() + $user['set_timesdvig'] * 60 * 60)) $timep = date("H:i:s", $time);
-		if ($time_p[0] == date("Y m d", time() - 60 * 60 * (24 - $user['set_timesdvig']))) $timep = "昨天$time_p[1]";
+		if ($time_p[0] == date("Y/m/d", time() + $user['set_timesdvig'] * 60 * 60)) $timep = date("H:i:s", $time);
+		if ($time_p[0] == date("Y/m/d", time() - 60 * 60 * (24 - $user['set_timesdvig']))) $timep = "昨天$time_p[1]";
 	} else {
-		if ($time_p[0] == date("Y m d")) $timep = date("H:i:s", $time);
-		if ($time_p[0] == date("Y m d", time() - 60 * 60 * 24)) $timep = "昨天$time_p[1]";
+		if ($time_p[0] == date("Y/m/d")) $timep = date("H:i:s", $time);
+		if ($time_p[0] == date("Y/m/d", time() - 60 * 60 * 24)) $timep = "昨天$time_p[1]";
 	}
 	$timep = str_replace("Jan", "1", $timep);
 	$timep = str_replace("Feb", "2", $timep);
