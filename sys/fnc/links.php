bb20cb531da0a471eb1689b5dcb7b5e2f0e35cdc
guguan123 <guguanming@outlook.com>
2025-03-28 17:44:03 +0800

    添加注释

--- sys/fnc/links.php
+++ sys/fnc/links.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * 处理带有 [url] 标签的链接并生成 HTML 锚标签
+ *
+ * 该函数通过正则表达式匹配处理传入的数组，判断链接是否为当前主机地址，
+ * 并根据全局配置 $set['web'] 决定是否添加 target="_blank" 属性。
+ *
+ * @param array $arr 包含链接和文本的数组，$arr[1] 为链接地址，$arr[2] 为链接文本
+ * @return string 返回生成的 HTML 锚标签字符串
+ */
 function links_preg1($arr) {
 	global $set;
 	if (preg_match('#^https://' . preg_quote($_SERVER['HTTP_HOST']) . '#', $arr[1]) || !preg_match('#://#', $arr[1])) {
@@ -9,6 +18,16 @@ function links_preg1($arr) {
 	}
 }
 
+/**
+ * 处理普通文本中的 URL 并生成 HTML 锚标签
+ *
+ * 该函数通过正则表达式匹配处理传入的数组，判断链接是否为当前主机地址（http 或 https），
+ * 并根据全局配置 $set['web'] 决定是否添加 target="_blank" 属性。
+ * 对于外部链接，会使用 base64 编码并跳转到 /go.php。
+ *
+ * @param array $arr 包含匹配文本的数组，$arr[1] 为前置文本，$arr[2] 为链接地址，$arr[4] 为后置文本
+ * @return string 返回生成的 HTML 字符串
+ */
 function links_preg2($arr) {
 	global $set;
 	if (preg_match('#^https://' . preg_quote($_SERVER['HTTP_HOST']) . '#', $arr[2])) {
@@ -21,6 +40,15 @@ function links_preg2($arr) {
 	}
 }
 
+/**
+ * 将文本中的 URL 转换为 HTML 锚标签
+ *
+ * 该函数根据全局配置 $set['bb_url'] 和 $set['bb_http']，分别处理 [url] 标签和普通文本中的链接，
+ * 并调用 links_preg1 和 links_preg2 函数进行具体转换。
+ *
+ * @param string $msg 输入的原始文本
+ * @return string 返回处理后的文本，包含 HTML 锚标签
+ */
 function links($msg) {
 	global $set;
 	if ($set['bb_url']) $msg = preg_replace_callback('/\[url=((?!javascript:|data:|document.cookie).+)\](.+)\[\/url\]/isU', 'links_preg1', $msg);
