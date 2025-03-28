2d2a2980c097c0a405139aa4c061746ce69b5f69
guguan123 <guguanming@outlook.com>
2025-03-28 18:48:48 +0800

    优化代码

--- go.php
+++ go.php
@@ -8,18 +8,15 @@ include_once 'sys/inc/db_connect.php';
 include_once 'sys/inc/ipua.php';
 include_once 'sys/inc/fnc.php';
 include_once 'sys/inc/user.php';
-$set['title']='外部链接跳转提示';
+$set['title'] = '外部链接跳转';
 include_once 'sys/inc/thead.php';
 title();
 
 if (!isset($_GET['go']) || (dbresult(dbquery("SELECT COUNT(*) FROM `rekl` WHERE `id` = '" . intval($_GET['go']) . "'"), 0) == 0 && !preg_match('#^https?://#', @base64_decode($_GET['go'])))) {
 	header("Location: index.php?" . session_id());
-	exit;
-}
-if (preg_match('#^(ht|f)tps?://#', base64_decode($_GET['go']))) {
+} elseif (preg_match('#^(ht|f)tps?://#', base64_decode($_GET['go']))) {
 	if (isset($_SESSION['adm_auth'])) unset($_SESSION['adm_auth']);
 	header("Location: " . base64_decode($_GET['go']));
-	exit;
 } else {
 	$rekl = dbassoc(dbquery("SELECT * FROM `rekl` WHERE `id` = '" . intval($_GET['go']) . "'"));
 	dbquery("UPDATE `rekl` SET `count` = '" . ($rekl['count'] + 1) . "' WHERE `id` = '{$rekl['id']}'");
@@ -31,4 +28,5 @@ if (preg_match('#^(ht|f)tps?://#', base64_decode($_GET['go']))) {
 	echo "你访问的链接是：<b><a href=\"{$rekl['link']}\">{$rekl['link']}</a></b><br />";
 	echo "访问次数: {$rekl['count']}<br />";
 }
+
 include_once 'sys/inc/tfoot.php';
