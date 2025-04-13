a1263a9933cb41f25827baa8ad470fe4607d4b58
guguan123 <guguanming@outlook.com>
2025-04-13 11:14:33 +0800

    修复 zzhy 的 bug（

--- user/money/liders.php
+++ user/money/liders.php
@@ -1,105 +1,78 @@
-<?
-require_once '../sys/inc/start.php';
-require_once '../sys/inc/compress.php';
-require_once '../sys/inc/sess.php';
-require_once '../sys/inc/home.php';
-require_once '../sys/inc/settings.php';
-require_once '../sys/inc/db_connect.php';
-require_once '../sys/inc/ipua.php';
-require_once '../sys/inc/fnc.php';
-require_once '../sys/inc/adm_check.php';
-include_once '../sys/inc/user.php';
+<?php
+require_once '../../sys/inc/start.php';
+require_once '../../sys/inc/compress.php';
+require_once '../../sys/inc/sess.php';
+require_once '../../sys/inc/home.php';
+require_once '../../sys/inc/settings.php';
+require_once '../../sys/inc/db_connect.php';
+require_once '../../sys/inc/ipua.php';
+require_once '../../sys/inc/fnc.php';
+require_once '../../sys/inc/adm_check.php';
+require_once '../../sys/inc/user.php';
 $set['title'] = '加入优先展示';
-require_once '../sys/inc/thead.php';
+require_once '../../sys/inc/thead.php';
 title();
-if (!isset($user))
-header("location: /index.php?");
+
+if (!isset($user)) header("location: ../../index.php?");
 err();
 aut();
-if (isset($user))
-{
-if (isset($_POST['stav']) && isset($_POST['msg']))
-{
-if ($_POST['stav']==1)
-{
-	$st=1;
-	$tm=$time+86400;
-}
-else if ($_POST['stav']==2)
-{
-	$st=2;
-	$tm=$time+172800;
-}
-else if ($_POST['stav']==3)
-{
-	$st=3;
-	$tm=$time+259200;
-}
-else if ($_POST['stav']==4)
-{
-	$st=4;
-	$tm=$time+345600;
-}
-else if ($_POST['stav']==5)
-{
-	$st=5;
-	$tm=$time+432000;
-}
-else if ($_POST['stav']==6)
-{
-	$st=6;
-	$tm=$time+518400;
-}
-else if ($_POST['stav']==7)
-{
-	$st=7;
-	$tm=$time+604800;
-}
-$msg=my_esc($_POST['msg']);
-if ($user['money']>=$st)
-{
-if (dbresult(dbquery("SELECT COUNT(*) FROM `liders` WHERE `id_user` = '$user[id]'"), 0)==0)
-{
-	dbquery("INSERT INTO `liders` (`id_user`, `stav`, `msg`, `time`, `time_p`) values('$user[id]', '$st', '".$msg."', '$tm', '$time')");
-}
-else
-{
-	dbquery("UPDATE `liders` SET `time` = '$tm', `time_p` = '$time', `msg` = '$msg', `stav` = '$st' WHERE `id_user` = '$user[id]'");
-}
-dbquery("UPDATE `user` SET `money` = '".($user['money']-$st)."' WHERE `id` = '$user[id]' LIMIT 1");
-$_SESSION['message'] = '你已经成功地成为一个领导者';
-header("Location: /user/liders/index.php?ok");
-exit;
-}else{
-$err='你没有足够的资金';
-}
-}else{
-$err='信息字段不能为空';
+
+if (isset($user)):
+if (isset($_POST['stav']) && isset($_POST['msg']) && is_int($_POST['stav'])) {
+	$st = $_POST['stav'];
+	$tm = $time + 60 * 60 * 24 * $_POST['stav'];
+	$msg = my_esc($_POST['msg']);
+	if ($user['money'] >= $st) {
+		if (dbresult(dbquery("SELECT COUNT(*) FROM `liders` WHERE `id_user` = '$user[id]'"), 0) == 0) {
+			dbquery("INSERT INTO `liders` (`id_user`, `stav`, `msg`, `time`, `time_p`) values('$user[id]', '$st', '$msg', '$tm', '$time')");
+		} else {
+			dbquery("UPDATE `liders` SET `time` = '$tm', `time_p` = '$time', `msg` = '$msg', `stav` = '$st' WHERE `id_user` = '$user[id]'");
+		}
+		dbquery("UPDATE `user` SET `money` = '" . ($user['money'] - $st) . "' WHERE `id` = '$user[id]' LIMIT 1");
+		$_SESSION['message'] = '你已经成功地成为一个领导者';
+		header("Location: ./index.php?ok");
+		exit;
+	} else {
+		$err = '你没有足够的资金';
+	}
+} else {
+	$err = '信息字段不能为空';
 }
 err();
-echo '<div class="foot">';
-echo '<img src="/style/icons/str2.gif" alt="S"/> <a href="/user/money/">附加服务</a> | <b>成为领导者</b>';
-echo '</div>';
-echo '<div class="mess">';
-echo '至少需要 <b style="color:red;">1</b> <b style="color:green;">' . $sMonet[1] . '</b>
-你的个人资料将在在线列表优先展示！'; 
-echo '</div>';
-echo '<form class="main" method="post" action="?">';
-	echo '花费: <select name="stav">
-	<option value="1">1</option>
-	<option value="2">2</option>
-	<option value="3">3</option>
-	<option value="4">4</option>
-	<option value="5">5</option>
-	<option value="6">6</option>
-	<option value="7">7</option>
-	</select> ' . $sMonet[0] . '<br />';
-echo '留言（215 字节）<textarea name="msg"></textarea><br />';
-echo '<input value="加入优先展示" type="submit" />';
-echo '</form>';
-}
-echo '<div class="foot">';
-echo '<img src="/style/icons/str2.gif" alt="S"/> <a href="/user/money/">附加服务</a>';
-echo '</div>';
-require_once '../sys/inc/tfoot.php';
 ?>
+
+<div class="foot">
+	<img src="../../style/icons/str2.gif" alt="S"/> <a href="./">附加服务</a> | <b>成为领导者</b>
+</div>
+<div class="mess">
+	至少需要<b style="color:red;"> 1 </b>
+	<b style="color:green;"><?php echo $sMonet[1]; ?></b>
+	你的个人资料将在在线列表优先展示！
+</div>
+<form class="main" method="post" action="?">
+	花费: 
+	<select name="stav">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+	</select> 
+	<?php echo $sMonet[0]; ?>
+	<br />
+	留言（215 字节）
+	<textarea name="msg"></textarea>
+	<br />
+	<input value="加入优先展示" type="submit" />
+</form>
+
+<?php endif; ?>
+
+<div class="foot">
+	<img src="../../style/icons/str2.gif" alt="S"/> <a href="./">附加服务</a>
+</div>
+
+<?php
+require_once '../../sys/inc/tfoot.php';
--- user/money/translate.php
+++ user/money/translate.php
@@ -1,63 +1,64 @@
-<?//网页标题
-require_once '../sys/inc/start.php';
-require_once '../sys/inc/compress.php';
-require_once '../sys/inc/sess.php';
-require_once '../sys/inc/home.php';
-require_once '../sys/inc/settings.php';
-require_once '../sys/inc/db_connect.php';
-require_once '../sys/inc/ipua.php';
-require_once '../sys/inc/fnc.php';
-require_once '../sys/inc/user.php';
+<?
+require_once '../../sys/inc/start.php';
+require_once '../../sys/inc/compress.php';
+require_once '../../sys/inc/sess.php';
+require_once '../../sys/inc/home.php';
+require_once '../../sys/inc/settings.php';
+require_once '../../sys/inc/db_connect.php';
+require_once '../../sys/inc/ipua.php';
+require_once '../../sys/inc/fnc.php';
+require_once '../../sys/inc/user.php';
 only_reg();
-if (isset($user))$ank['id'] = intval($_GET['id']);
-$ank=user::get_user($ank['id']);
-if(!$ank || $user['id'] == $ank['id']){header("Location: /index.php?".session_id());exit;}
-if (isset($_GET['act']) && $_POST['money'])
-{
-$money=abs(intval($_POST['money']));
-if ($user['money'] < $money)$err = '你没有足够的硬币赠送';
-if (!$err)
-{
-dbquery("UPDATE `user` SET `money` = '" . ($ank['money'] + $money) . "' WHERE `id` ='$ank[id]';");
-dbquery("UPDATE `user` SET `money` = '" . ($user['money'] - $money) . "' WHERE `id` ='$user[id]';");
-$msg = "用户 [b]".$user['nick']."[/b] 赠送给你 [b] $money [/b] $sMonet[0]! [br]别忘了说谢谢！";
-dbquery("INSERT INTO `mail` (`id_user`, `id_kont`, `msg`, `time`) values('0', '$ank[id]', '$msg', '$time')");
-$_SESSION['message'] = '赠送成功';
-header("Location: /user/info.php?id=$ank[id]");
-exit;
+
+if (isset($user)) $ank['id'] = intval($_GET['id']);
+$ank = user::get_user($ank['id']);
+
+if(!$ank || $user['id'] == $ank['id']) {
+	header("Location: ../../index.php?" . session_id());
+	exit;
 }
+
+if (isset($_GET['act']) && $_POST['money']) {
+	$money=abs(intval($_POST['money']));
+	if ($user['money'] < $money) $err = '你没有足够的硬币赠送';
+	if (!$err) {
+		dbquery("UPDATE `user` SET `money` = '" . ($ank['money'] + $money) . "' WHERE `id` ='$ank[id]';");
+		dbquery("UPDATE `user` SET `money` = '" . ($user['money'] - $money) . "' WHERE `id` ='$user[id]';");
+		$msg = "用户 [b]{$user['nick']}[/b] 赠送给你 [b]{$money}[/b] {$sMonet[0]}! [br]别忘了说谢谢！";
+		dbquery("INSERT INTO `mail` (`id_user`, `id_kont`, `msg`, `time`) values('0', '$ank[id]', '$msg', '$time')");
+		$_SESSION['message'] = '赠送成功';
+		header("Location: ../info.php?id={$ank['id']}");
+		exit;
+	}
 }
-$set['title']='赠送'.$sMonet[0]; // заголовок страницы
-require_once '../sys/inc/thead.php';
+
+$set['title'] = '赠送' . $sMonet[0];
+require_once '../../sys/inc/thead.php';
 title();
 aut();
 err();
+
 echo "<div class='foot'>";
-echo "<img src='/style/icons/str2.gif' alt='*'> <a href='/user/info.php?id=$ank[id]'>$ank[nick]</a> | 赠送<br />";
+echo "<img src='../../style/icons/str2.gif' alt='*'> <a href='../info.php?id={$ank['id']}'>{$ank['nick']}</a> | 赠送<br />";
 echo "</div>";
-if (isset($user) & $user['money']<=1)
-{
-echo '<div class="mess">';
-	if ($user['pol']==0){
-		echo "<b>不好意思，美女，</b> ";
-	} else {
-		echo "<b>不好意思，兄弟，</b> ";
-	}
-		echo "赠送$sMonet[2]给其他用户需要获得最低 <b>2</b> $sMonet[2]。<br/>你目前只有 <b>$user[money] </b>$sMonet[0]。";
-echo '</div>';
+
+if (isset($user) & $user['money']<=1) {
+	echo '<div class="mess">';
+	echo "赠送$sMonet[2]给其他用户需要获得最低 <b>2</b> $sMonet[2]。<br/>你目前只有 <b>$user[money] </b>$sMonet[0]。";
+	echo '</div>';
+} else {
+	echo '<div class="mess">';
+	echo "你的 $sMonet[2]: <b>$user[money]</b><br />";
+	echo '</div>';
+	echo "<form class='main' action=\"?id=$ank[id]&amp;act\" method=\"post\">";
+	echo "数量 $sMonet[0]:<br />";
+	echo "<input type='text' name='money' value='1' /><br />";
+	echo "<input class='submit' type='submit' value='赠送' /><br />";
+	echo "</form>";
 }
-else
-{
-echo '<div class="mess">';
-echo "你的 $sMonet[2]: <b>$user[money]</b><br />";
-echo '</div>';
-echo "<form class='main' action=\"?id=$ank[id]&amp;act\" method=\"post\">";
-echo "数量 $sMonet[0]:<br />";
-echo "<input type='text' name='money' value='1' /><br />";
-echo "<input class='submit' type='submit' value='赠送' /><br />";
-echo "</form>";
-}
+
 echo "<div class='foot'>";
-echo "<img src='/style/icons/str2.gif' alt='*'> <a href='/user/info.php?id=$ank[id]'>$ank[nick]</a> | 赠送<br />";
+echo "<img src='../../style/icons/str2.gif' alt='*'> <a href='../info.php?id=$ank[id]'>$ank[nick]</a> | 赠送<br />";
 echo "</div>";
-require_once '../sys/inc/tfoot.php';
+
+require_once '../../sys/inc/tfoot.php';
