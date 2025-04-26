5a8678673f747e33725f893336a42b500807489b
guguan123 <guguanming@outlook.com>
2025-04-26 08:51:52 +0800

    添加注释

--- install/db_tables/chat_rooms.sql
+++ install/db_tables/chat_rooms.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `chat_rooms` (
-  `id` int(11) NOT NULL auto_increment,
-  `pos` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `umnik` set('0','1') default '0',
-  `shutnik` set('0','1') default '0',
-  `opis` varchar(256) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `pos` (`pos`,`umnik`,`shutnik`)
-) ENGINE=MyISAM DEFAULT CHARSET  utf8mb4 COLLATE  utf8mb4_unicode_ci;
+	`id` int(11) NOT NULL auto_increment,	-- 聊天室 ID
+	`pos` int(11) NOT NULL,								-- 排序用的 ID （越小越靠前）
+	`name` varchar(32) NOT NULL,					-- 聊天室名称
+	`umnik` set('0','1') default '0',			-- 是否启用答题机器人
+	`shutnik` set('0','1') default '0',		-- 是否启用笑话机器人
+	`opis` varchar(256) NOT NULL,					-- 聊天室描述
+	PRIMARY KEY  (`id`),
+	KEY `pos` (`pos`,`umnik`,`shutnik`)
+);
