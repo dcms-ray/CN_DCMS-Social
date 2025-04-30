e71f56c70fefb892ed16f73b8fbbb604daf04ad5
guguan123 <guguanming@outlook.com>
2025-04-30 20:30:55 +0800

    renamed:    sys/inc/downloadfile.php -> sys/fnc/downloadfile.php

--- photo/img.php
+++ photo/img.php
@@ -6,7 +6,6 @@ include_once '../sys/inc/settings.php';
 include_once '../sys/inc/db_connect.php';
 include_once '../sys/inc/ipua.php';
 include_once '../sys/inc/fnc.php';
-include_once '../sys/inc/downloadfile.php';
 include_once '../sys/inc/user.php';
 
 header("Expires: ".gmdate("D, d M Y H:i:s", time() + 3600)." GMT");
