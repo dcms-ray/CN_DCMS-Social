383bd08b71623a5523ef18f31120002c73850875
guguan123 <guguanming@outlook.com>
2025-05-25 01:16:03 +0800

    修复部分兼容性问题

--- guest/inc/admin_form.php
+++ guest/inc/admin_form.php
@@ -1,7 +1,6 @@
 <?php
-if (user_access('guest_clear')) {
-	if (isset($_GET['act']) && $_GET['act'] == 'create') {
-		?>
+if (user_access('guest_clear')):
+	if (isset($_GET['act']) && $_GET['act'] == 'create'): ?>
 		<form method="post" class="nav2" action="?">
 		将删除撰写的帖子...返回<br />
 		<input name="write" value="12" type="text" size="3" />
@@ -12,11 +11,8 @@ if (user_access('guest_clear')) {
 		</select><br />
 		<input value="清除" type="submit" /> <a href="?">取消</a><br />
 		</form>
-		<?
-	}
-	?>
+		<?php endif; ?>
 	<div class="foot">
 	<img src='../style/icons/str.gif' alt='*'> <a href="?act=create">清除留言</a><br />
 	</div>
-	<?
-}
+<?php endif;
