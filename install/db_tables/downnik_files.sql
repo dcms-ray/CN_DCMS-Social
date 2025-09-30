0a0323d87d03efd150ae067a5956680b8f9adad0
guguan123 <guguanming@outlook.com>
2025-10-01 00:13:33 +0800

    增大type字段

--- install/db_tables/downnik_files.sql
+++ install/db_tables/downnik_files.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `downnik_files` (
   `id_dir` int(11) NOT NULL,
   `name` varchar(128) CHARSET utf8mb4 COLLATE  utf8mb4_unicode_ci NOT NULL,
   `ras` varchar(36) NOT NULL,
-  `type` varchar(64) NOT NULL,
+  `type` varchar(256) NOT NULL,
   `time` int(11) NOT NULL,
   `time_last` int(11) NOT NULL,
   `size` int(11) NOT NULL,
