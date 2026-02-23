91e86de738def7371f10f9ba4349cc49d8534973
GuGuan123 <admin@guguan.us.kg>
2026-02-23 22:03:02 +0800

    显示SQL错误详情

--- install/inc/ver_tables.php
+++ install/inc/ver_tables.php
@@ -12,13 +12,19 @@ while ($filetables = readdir($opdirtables)) {
 	if (preg_match('#\.sql$#i', $filetables)) {
 		$table_name = preg_replace('#\.sql$#i', '', $filetables);
 		if (!isset($_ver_table[$table_name])) {
-			echo '正在创建表: ' . $table_name . '<br>';
+			echo '正在创建表: ' . $table_name . ' (' . $filetables . ')<br>';
 			include_once check_replace(H.'sys/inc/sql_parser.php');
 			$sql = SQLParser::getQueriesFromFile(H . 'install/db_tables/' . $filetables);
 			for ($i = 0; $i < count($sql); $i++) {
 				$k_sql++; // 查询计数器（用于安装程序）
 				if (mysqli_query($mydb, $sql[$i])) {
 					$ok_sql++; // 成功查询计数器（用于安装程序）
+				} else {
+					// 如果执行失败，直接报错并抓出文件名
+					die('<br><strong style="color:red;">哎呀！SQL 执行出错啦！</strong><br>' .
+						'错误文件: <b>install/db_tables/' . $filetables . '</b><br>' .
+						'具体错误: ' . mysqli_error($mydb) . '<br>' .
+						'出错的 SQL 语句: <pre>' . $sql[$i] . '</pre>');
 				}
 			}
 		}
