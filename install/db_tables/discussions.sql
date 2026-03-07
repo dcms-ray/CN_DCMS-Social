3f39aada828a7245b96bae2009d4bfcf4d473b29
GuGuan123 <guguanming@outlook.com>
2026-03-07 23:22:55 +0800

    加索引

--- install/db_tables/discussions.sql
+++ install/db_tables/discussions.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS `discussions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `avtor` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `count` int(11) DEFAULT '0',
-  `msg` varchar(1024),
-  `time` int(11) NOT NULL,
-  `type` varchar(100) NOT NULL,
-  `id_sim` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`avtor` int(11) NOT NULL,
+	`id_user` int(11) NOT NULL,
+	`count` int(11) DEFAULT '0',
+	`msg` varchar(1024),
+	`time` int(11) NOT NULL,
+	`type` varchar(100) NOT NULL,
+	`id_sim` int(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `idx_user_type_sim` (`id_user`, `type`, `id_sim`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
