07dfb860f987a89cb5d6337c34c0987ab896212f
GuGuan123 <guguanming@outlook.com>
2026-03-07 21:45:24 +0800

    踢飞短标签

--- sys/inc/adm_check.php
+++ sys/inc/adm_check.php
@@ -1,14 +1,10 @@
-<?
-function adm_check()
-{
-	global $time;
-	if (!isset($_SESSION['adm_auth']) || $_SESSION['adm_auth'] < $time)
-	{
-		header('Location: /adm_panel/?go=' . base64_encode($_SERVER['REQUEST_URI']) . '&' . passgen() . '&' . session_id()); exit;
+<?php
+function adm_check() {
+	global $time, $set;
+	if (!isset($_SESSION['adm_auth']) || $_SESSION['adm_auth'] < $time) {
+		header('Location: ' . $set['siteurl'] . '/adm_panel/?go=' . base64_encode($_SERVER['REQUEST_URI']) . '&' . passgen() . '&' . session_id()); exit;
 	}
-	if (isset($_SESSION['adm_auth']) && $_SESSION['adm_auth']>$time)
-	{
-		$_SESSION['adm_auth'] = $time + setget("timeadmin",1000);
+	if (isset($_SESSION['adm_auth']) && $_SESSION['adm_auth'] > $time) {
+		$_SESSION['adm_auth'] = $time + setget("timeadmin", 1000);
 	}
 }
-?>
