968d74a3aaa595378f792a65e3dfd4b917746ec2
guguan123 <93070974+guguan123@users.noreply.github.com>
2026-03-09 04:35:01 +0800

    添加ID参数

--- user/mail.php
+++ user/mail.php
@@ -181,7 +181,7 @@ if (isset($_GET['delete']) && $_GET['delete'] != 'all') {
 		echo '<div class="nav1">';
 		echo $mess['msg'];
 		echo '</div>';
-		echo "<form method='post' name='delete' action='?delete={$mess['id']}'>";
+		echo '<form method="post" name="delete" action="?id=' . $ank['id'] . '&amp;delete=' . $mess['id'] . '">';
 		echo '<input type="hidden" name="continue" value="yes">';
 		echo '<input value="确认删除" type="submit">';
 		echo '</form>';
@@ -200,7 +200,7 @@ if (isset($_GET['delete']) && $_GET['delete'] == 'all') {
 		echo '<div class="nav1">';
 		echo $mess['msg'];
 		echo '</div>';
-		echo "<form method='post' name='delete' action='?delete=all'>";
+		echo '<form method="post" name="delete" action="?id=' . $ank['id'] . '&amp;delete=all">';
 		echo '<input type="hidden" name="continue" value="ok">';
 		echo '<input value="确认删除" type="submit">';
 		echo '</form>';
