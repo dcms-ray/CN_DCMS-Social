8ad3ddf3f0d1a6e57853ce58a7706bcf2bf4e3ba
guguan123 <93070974+guguan123@users.noreply.github.com>
2026-03-21 23:09:27 +0800

    相对路径

--- install/inc/head.php
+++ install/inc/head.php
@@ -6,11 +6,11 @@ header("Content-type: application/xhtml+xml;charset=UTF-8");
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
 <head>
 	<title><?echo $set['title'];?></title>
-	<link rel="stylesheet" href="/style/themes/<?php echo $set['set_them']; ?>/style.css" type="text/css" />
+	<link rel="stylesheet" href="../style/themes/<?php echo $set['set_them']; ?>/style.css" type="text/css" />
 </head>
 <body>
 	<div class="body">
-	<div class="logo"><img src="/style/themes/default/logo.png"  alt="Logotype" /><br />
+	<div class="logo"><img src="../style/themes/default/logo.png"  alt="Logotype" /><br />
 	DCMS Social - 社交网络引擎</div>
 	<div class="title">
 <?php
