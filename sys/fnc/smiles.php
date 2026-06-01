484aad55e0ce22bcc854490dfa6980e3af0dae98
GuGuan123 <guguanming@outlook.com>
2026-06-01 20:11:50 +0800

    使用$set['siteurl']

--- sys/fnc/smiles.php
+++ sys/fnc/smiles.php
@@ -5,7 +5,7 @@ function smiles($msg) {
 	while($post = dbarray($q)) {
 		$sm = explode("|", $post['smile']);
 		for ($i = 0; $i < count($sm); $i++) {
-			$msg = str_replace($sm[$i], '<img src="/files/smiles/' . $post['id'] . '.gif" alt="smile" />', $msg);
+			$msg = str_replace($sm[$i], '<img src="' . $set['siteurl'] . '/files/smiles/' . $post['id'] . '.gif" alt="smile" />', $msg);
 		}
 	}
 	return $msg;
