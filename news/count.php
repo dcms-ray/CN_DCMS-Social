c335d2f53753879e951699fd0f9610e7876e8e3b
GuGuan123 <admin@guguan.us.kg>
2026-06-30 11:56:10 +0800

    替换掉旧的数据库查询函数

--- news/count.php
+++ news/count.php
@@ -1,6 +1,6 @@
 <?php
-$k_p = dbresult(dbquery("SELECT COUNT(*) FROM `news`"), 0);
-$k_n = dbresult(dbquery("SELECT COUNT(*) FROM `news` WHERE `time` > '" . mktime(0, 0, 0) . "'"), 0);
+$k_p = $db->queryColumn('SELECT COUNT(*) FROM `news`');
+$k_n = $db->queryColumn('SELECT COUNT(*) FROM `news` WHERE `time` > ?', [mktime(0, 0, 0)]);
 if ($k_n == 0) {
 	$k_n = NULL;
 } else {
