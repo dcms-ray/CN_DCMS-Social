51f8ceb387f43a1d1e8c3879e10166e944549412
GuGuan123 <admin@guguan.us.kg>
2026-06-30 12:03:26 +0800

    替换掉旧的数据库查询函数

--- down/count.php
+++ down/count.php
@@ -1,7 +1,7 @@
 <?php
-$my_dir = dbassoc(dbquery("SELECT * FROM `downnik_dir` WHERE `my` = '1' LIMIT 1"));
-$k_p = dbresult(dbquery("SELECT COUNT(*) FROM `downnik_files` WHERE `id_dir` != '$my_dir[id]'"), 0);
-$k_n = dbresult(dbquery("SELECT COUNT(*) FROM `downnik_files` WHERE `id_dir` != '$my_dir[id]' AND `time_go` > '" . mktime(0, 0, 0) . "'"), 0);
+$my_dir = $db->queryColumn("SELECT * FROM `downnik_dir` WHERE `my` = '1' LIMIT 1");
+$k_p = $db->queryColumn('SELECT COUNT(*) FROM `downnik_files` WHERE `id_dir` != ?', [$my_dir['id']]);
+$k_n = $db->queryColumn('SELECT COUNT(*) FROM `downnik_files` WHERE `id_dir` != ? AND `time_go` > ?', [$my_dir['id'], mktime(0, 0, 0)]);
 if ($k_n == 0) {
 	$k_n = NULL;
 } else {
