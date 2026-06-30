7bacaa5850ccf36842047d44b9d116b9a1e6f015
GuGuan123 <admin@guguan.us.kg>
2026-06-30 12:19:27 +0800

    重构 chat\count.php

--- chat/count.php
+++ chat/count.php
@@ -1,5 +1,4 @@
 <?php
-// 用于菜单栏的在线用户计数
-if (isset($user) && empty($in_chat_room)) dbquery("DELETE FROM `chat_who` WHERE `id_user` = '$user[id]'");
-dbquery("DELETE FROM `chat_who` WHERE `time` < '" . ($time - 120) . "'");
-echo '(' . dbresult(dbquery("SELECT COUNT(*) FROM `chat_who`"), 0) . ' 人)';
+if (isset($user) && empty($in_chat_room)) $db->executeStatement('DELETE FROM `chat_who` WHERE `id_user` = ?', [$user['id']]);
+$db->executeStatement('DELETE FROM `chat_who` WHERE `time` < ?', [time() - 120]);
+echo '(' . $db->queryColumn('SELECT COUNT(*) FROM `chat_who`') ?: 0 . ' 人)';
