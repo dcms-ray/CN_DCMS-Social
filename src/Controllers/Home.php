d38318d79b3b65fb5fd97533be69e855dcf34837
GuGuan123 <admin@guguan.us.kg>
2026-06-30 11:46:23 +0800

    重构 sys/inc/main_menu.php

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -24,17 +24,41 @@ class Home
 			$news = $db->query('SELECT * FROM `news` WHERE `main_time` > ? ORDER BY `id` DESC LIMIT 1', [time()]);
 			if ($news !== null && !$set->get('web') && (empty($user) || $user['news_read'] == 0)) {
 				echo '<div class="mess">';
-				echo '<img src="/style/icons/blogi.png" alt="*" /> <a href="/news/news.php?id=' . $news['id'] . '">' . text($news['title']) . '</a><br/> ';
+				echo '<img src="style/icons/blogi.png" alt="*" /> <a href="news/news.php?id=' . $news['id'] . '">' . text($news['title']) . '</a><br/> ';
 				echo output_text($news['msg']) . '<br />';
 				if ($news['link'] != NULL) echo '<a href="' . htmlentities($news['link'], ENT_QUOTES, 'UTF-8') . '">详情</a><br />';
 				echo '作者: ' . \GuGuan123\dcms\Utils\user::nick($news['id_user'], 1, 1, 0) . ' ' . vremja($news['time']) . ' ';
-				echo ' <img src="/style/icons/komm.png" alt="*" /> (' . $db->queryColumn('SELECT COUNT(*) FROM `news_komm` WHERE `id_news` = ?', [$news['id']]) . ')<br />';
+				echo ' <img src="style/icons/komm.png" alt="*" /> (' . $db->queryColumn('SELECT COUNT(*) FROM `news_komm` WHERE `id_news` = ?', [$news['id']]) . ')<br />';
 				if (isset($user)) echo '<div style="text-align:right;"><a href="?news_read">隐藏</a></div>';
 				echo '</div>';
 			}
 
 			// 主菜单
-			include 'sys/inc/main_menu.php'; 
+			$menuList = $db->queryAll("SELECT * FROM `menu` ORDER BY `pos` ASC") ?: [];
+			foreach ($menuList as $post_menu) {
+				if ($post_menu['type'] == 'link') {
+					echo '<div class="main_menu">';
+					echo '<img src="style/icons/' . htmlspecialchars($post_menu['icon']) . '" alt="*" /> ';
+					echo '<a href="' . htmlspecialchars($post_menu['url']) . '">' . htmlspecialchars($post_menu['name']) . '</a> ';
+				} else {
+					// 如果不是 link，那就是 'razd'
+					echo '<div class="menu_razd">';
+					echo htmlspecialchars($post_menu['name']);
+				}
+				// 动态引入计数器文件
+				if ($post_menu['counter'] != NULL && test_file($post_menu['counter'])) {
+					include $post_menu['counter'];
+				}
+				echo '</div>';
+			}
+
+			// 网站管理面板
+			if (user_access('adm_panel_show')) {
+				echo '<div class="main2">';
+				echo '<img src="style/icons/adm.gif" alt="DS" /> <a href="plugins/admin/">网站管理</a> ';
+				include_once check_replace('plugins/admin/count.php');
+				echo '</div>';
+			}
 			include 'sys/inc/main_notes.php';
 		} else {
 			// 主要网页主题
