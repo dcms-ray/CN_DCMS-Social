83e75b4c67a051aa453b59fe0b0df20576fb581c
GuGuan123 <admin@guguan.us.kg>
2026-06-30 11:41:18 +0800

    重构 sys/inc/news_main.php

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -19,8 +19,20 @@ class Home
 			// 在线游客数量
 			$ol_guest = $db->queryColumn('SELECT COUNT(*) FROM `guests` WHERE `date_last` > ? AND `pereh` > ?', [time() - 600, 0]);
 			include 'resources/assets/index.php';
+
 			// 新闻&事件
-			include 'sys/inc/news_main.php'; 
+			$news = $db->query('SELECT * FROM `news` WHERE `main_time` > ? ORDER BY `id` DESC LIMIT 1', [time()]);
+			if ($news !== null && !$set->get('web') && (empty($user) || $user['news_read'] == 0)) {
+				echo '<div class="mess">';
+				echo '<img src="/style/icons/blogi.png" alt="*" /> <a href="/news/news.php?id=' . $news['id'] . '">' . text($news['title']) . '</a><br/> ';
+				echo output_text($news['msg']) . '<br />';
+				if ($news['link'] != NULL) echo '<a href="' . htmlentities($news['link'], ENT_QUOTES, 'UTF-8') . '">详情</a><br />';
+				echo '作者: ' . \GuGuan123\dcms\Utils\user::nick($news['id_user'], 1, 1, 0) . ' ' . vremja($news['time']) . ' ';
+				echo ' <img src="/style/icons/komm.png" alt="*" /> (' . $db->queryColumn('SELECT COUNT(*) FROM `news_komm` WHERE `id_news` = ?', [$news['id']]) . ')<br />';
+				if (isset($user)) echo '<div style="text-align:right;"><a href="?news_read">隐藏</a></div>';
+				echo '</div>';
+			}
+
 			// 主菜单
 			include 'sys/inc/main_menu.php'; 
 			include 'sys/inc/main_notes.php';
